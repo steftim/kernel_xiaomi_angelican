023c2dd852a3b0dd919fb7b357e6ad5c758caa3f
FlyFrog <ramgear@gmail.com>
2022-07-03 10:41:07 +0300

    int_sqrt: Improve 3x faster integer sqrt.
    Result on 10,000,000 call.
    Old:
    sqrt(12345689) = 3513
    real	0m0.768s
    user	0m0.760s
    sys	0m0.004s
    
    New:
    sqrt(12345689) = 3513
    real	0m0.222s
    user	0m0.224s
    sys	0m0.000s

--- lib/int_sqrt.c
+++ lib/int_sqrt.c
@@ -1,39 +1,46 @@
+// SPDX-License-Identifier: GPL-2.0
 /*
  * Copyright (C) 2013 Davidlohr Bueso <davidlohr.bueso@hp.com>
  *
  *  Based on the shift-and-subtract algorithm for computing integer
  *  square root from Guy L. Steele.
  */
-
 #include <linux/kernel.h>
 #include <linux/export.h>
 #include <linux/bitops.h>
-
 /**
- * int_sqrt - rough approximation to sqrt
+ * int_sqrt - computes the integer square root
  * @x: integer of which to calculate the sqrt
  *
- * A very rough approximation to the sqrt() function.
+ * Computes: floor(sqrt(x))
  */
-unsigned long int_sqrt(unsigned long x)
+inline unsigned long int_sqrt(unsigned long x)
 {
-	unsigned long b, m, y = 0;
+	register unsigned long tmp;
+	register unsigned long place;
+	register unsigned long root = 0;
 
 	if (x <= 1)
 		return x;
 
-	m = 1UL << (__fls(x) & ~1UL);
-	while (m != 0) {
-		b = y + m;
-		y >>= 1;
+	place = 1UL << (BITS_PER_LONG - 2);
 
-		if (x >= b) {
-			x -= b;
-			y += m;
+	do{
+		place >>= 2;
+	}while(place > x);
+
+	do {
+		tmp = root + place;
+		root >>= 1;
+
+		if (x >= tmp)
+		{
+			x -= tmp;
+			root += place;
 		}
-		m >>= 2;
-	}
+		place >>= 2;
+	}while (place != 0);
 
-	return y;
+	return root;
 }
 EXPORT_SYMBOL(int_sqrt);
